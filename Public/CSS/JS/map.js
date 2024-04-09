27117c39494aab608cd763ed1aa93fa504a550e4
Ronit Prasad <115333372+ronitprasad23@users.noreply.github.com>
2024-04-09 14:05:40 +0530

    Add files via upload

---
+++ Public/CSS/JS/map.js
@@ -0,0 +1,15 @@
+
+	mapboxgl.accessToken = mapToken;
+
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: [72.58727, 23.02579], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+    });
+
+    console.log(coordinates);
+    
+    const marker = new mapboxgl.Marker()
+    .setLngLat(coordinates)
+    .addTo(map);
