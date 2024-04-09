03cf2b642cad8079244273a694ee1f1757f2cbc9
Ronit Prasad <115333372+ronitprasad23@users.noreply.github.com>
2024-04-09 14:10:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+if(process.env.NODE_ENV != "production") {
+    require('dotenv').config();
+}
+
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./Models/listening");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const ExpressError = require("./Utils/ExpressError");
+const listingroute = require("./Routes/listing");
+const reviewroute = require("./Routes/review"); 
+const userroute = require("./Routes/user");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");   
+const LocalsStrategy = require("passport-local");
+const User = require("./Models/user");  
 
-const MONGO_DB = "mongodb://127.0.0.1:27017/VacationVilla";
+
+
+const MONGO_DB = "mongodb://127.0.0.1:27017/Vacation-Villa";
 
 main().then(() =>{
     console.log("connected to Database");
@@ -25,101 +40,64 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname,"/Public")));
 
+
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true, 
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+};
+
 app.get("/", (req,res)=> {
     res.send("Hii i am root");
 })
 
-//Index Route:
-app.get("/Listings", async (req,res) => {
-    const allListings =  await Listing.find({});
-    res.render("listings/index", {allListings});  
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalsStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
 });
 
-//New Route:
-app.get("/Listings/new", (req,res) => {
-    res.render("listings/new");
-})
+// app.get("/demouser", async (req,res) => {
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "stu-delta"
+//     });
 
-
-//Show Route:
-app.get("/Listings/:id", async (req,res) => {
-    let {id} = req.params;
-    const listings = await Listing.findById(id);
-    res.render("listings/show", {listings});
-});
-
-//Create Route:
-app.post("/Listings", async (req,res) => {
-    const newlisting = new Listing(req.body.listing);
-    await newlisting.save();
-    res.redirect("/Listings");
-})
-
-//Edit Route:
-app.get("/Listings/:id/edit", async (req,res) => {
-    let {id} = req.params;
-    const listings = await Listing.findById(id);
-    res.render("listings/edit", {listings});
-})
-
-//update route:
-app.put("/Listings/:id", async (req, res) => {
-    // const { id } = req.params;
-    // const { image, ...otherProps } = req.body.Listing;
-    // let existingListing = await Listing.findById(id);
-
-    // // Check if there's a new image provided
-    // if (image && image.url && image.filename) {
-    //     // Update image URL and filename
-    //     existingListing.image.url = image.url;
-    //     existingListing.image.filename = image.filename;
-    // }
-
-    // Update other properties
-//     Object.assign(existingListing, otherProps);
-
-//     await existingListing.save();
-//     res.redirect(`/Listings/${id}`);
-let {id}=req.params;// console.log(id,currUser._id,list.owner._id);
-        // console.log(req.body.listing);
-        let img={filename:"listingimage",url:req.body.listing.image}
-        let data={...req.body.listing,image:{...img}}
-        // console.log(data)
-        // let url=req.file.path;
-        // let filename=req.file.filename;
-        // if(req.file){
-        let list= await Listing.findByIdAndUpdate(id, data);
-        // list.image={url,filename};
-        // await list.save();
-        // }
-        // req.flash("success","Listing Updated Succesfully");
-        res.redirect(`/Listings/${id}`);
-     
-});
-
-//delete Route:
-app.delete("/Listings/:id", async (req,res) => {
-    let {id} = req.params;
-    let deletelisting = await Listing.findByIdAndDelete(id);
-    console.log(deletelisting);
-    res.redirect("/Listings");
-})
-
-
-// app.get("/testlisting", async (req,res) => {
-//     let sampleListing = new Listing({
-//         title: "Hill view villa",
-//         description: "Buy this amazing villa for your fun",
-//         price: 2000,
-//         location: "Mount Abu, Rajasthan",
-//         country: "India"
-//     }); 
-
-//     await sampleListing.save();
-//     console.log("Sample has been saved");
-//     res.send("Successfully Done");
+//     let registeredUser = await User.register(fakeUser, "stu-delta@234");
+//     console.log(registeredUser);
 // })
 
-app.listen(8080,() => {
+
+app.use("/Listings", listingroute);
+app.use("/Listings/:id/reviews", reviewroute);
+app.use("/", userroute);
+
+
+app.all("*", (req,res,next) => {
+    next(new ExpressError(404, "Page not found!"));
+});
+
+app.use((err,req,res,next) => {
+    let {statusCode=404, message="Something went wrong"} = err;
+    res.status(statusCode).render("error.ejs", {message});
+    // res.status(statusCode).send(message);
+});
+
+app.listen(8080,() => { 
     console.log("Server is listening to port 8080");
-})
+});
